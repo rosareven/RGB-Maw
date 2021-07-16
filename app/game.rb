37321d79c8db870b993c439d38e1e0cc83d86459
Rosareven <rosareven@gmail.com>
2021-07-16 16:10:39 +0100

    Added mouse over detection on the blocks

--- app/game.rb
+++ app/game.rb
@@ -4,16 +4,17 @@ controls.define :reset, keyboard: :r
 
 midx = (grid.w / 2) - 50
 blocky = grid.h - 200
+blocks = Array.new
 
 init {
     $state.background = [grid.rect, 236, 213, 121]
     $state.block = {
         x: midx, y: blocky, w: 100, h: 100, path:"sprites/square/white.png",
-        a: 255, r: 0, g: 0, b: 0
+        a: 255, r: 0, g: 0, b: 0, name: ""
     }
-    $state.green ||= $state.new_entity(:green, $state.block.merge(g: 255))
-    $state.red ||= $state.block.merge(x: midx/2, r: 255)
-    $state.blue ||= $state.block.merge(x: midx+(midx/2), b: 255)
+    blocks << $state.green ||= $state.block.merge(g: 255, name: "Green")
+    blocks << $state.red ||= $state.block.merge(x: midx/2, r: 255, name: "Red")
+    blocks << $state.blue ||= $state.block.merge(x: midx+(midx/2), b: 255, name: "Blue")
     
     $state.mouse_label = { x: 10, y: grid.h - 10 }
     $state.block_label = { x: grid.w/2, y: grid.h - 10, alignment_enum: 1}
@@ -27,7 +28,10 @@ tick {
     sprites << $state.blue
 
     labels << $state.mouse_label.merge!(text: "X: "+mouse.x.to_s+" Y: "+mouse.y.to_s)
-    if mouse.inside_rect? $state.green.rect
-        labels << $state.block_label.merge!(text: $state.green.entity_type.to_s)
+
+    blocks.each do |e|
+        if mouse.inside_rect? e.rect
+            labels << $state.block_label.merge!(text: e.name)
+        end
     end
 }
